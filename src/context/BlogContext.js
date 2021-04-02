a003f3d40d4e095a9ddb89a88f4424f6a01215bf
Gareth Gomersall <gareth.gomersall@justgiving.com>
2021-04-02 17:36:37 +0100

    added createDataContext file which will be our base context - makes everything more streamline

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,6 @@
-import React, { createContext, useReducer } from 'react';
+import React, { useReducer } from 'react';
 
-const BlogContext = createContext()
+import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
@@ -11,20 +11,13 @@ const blogReducer = (state, action) => {
   }
 }
 
-export const BlogProvider = ({children}) => {
-  const [blogPosts, dispatch] = useReducer(blogReducer, [])
-
-  const addBlogPost = () => {
-    dispatch({ type: 'add_blogpost'});
-  }
-
-  return (
-    <BlogContext.Provider 
-    value={{data: blogPosts, addBlogPost}}
-    >
-      {children}
-    </BlogContext.Provider>
-  )
+const addBlogPost = () => {
+  dispatch({ type: 'add_blogpost'});
 }
 
-export default BlogContext
+
+export const {Context, Provider} = createDataContext(
+  blogReducer, 
+  {addBlogPost},
+  []
+)
